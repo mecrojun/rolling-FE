4bcb2ce8278c971c2b11aebf59c9062d2636edbe
heresue <sue.synpark@gmail.com>
2025-02-03 07:09:50 +0900

    feat: ColorChip 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
+import Colorchip from "./components/Colorchip";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div>hello</div>
+      <Colorchip />
     </ThemeProvider>
   );
 }
