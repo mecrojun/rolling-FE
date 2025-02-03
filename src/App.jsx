682867c537ad5f2d443f36e1eb671a4326c9626b
heresue <sue.synpark@gmail.com>
2025-02-04 03:26:14 +0900

    chore: 임시 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
-import Colorchip from "./components/Colorchip";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div>hello</div>
-      <Colorchip />
     </ThemeProvider>
   );
 }
