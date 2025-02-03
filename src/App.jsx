4886c11b28dfa076e4cd604724a14de32fda6656
heresue <sue.synpark@gmail.com>
2025-02-04 02:29:38 +0900

    chore: 임시 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
-import { EmojiBadge, RelBadge } from "./components/Badge";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div>hello</div>
-      <RelBadge />
-      <EmojiBadge count={24} />
     </ThemeProvider>
   );
 }
