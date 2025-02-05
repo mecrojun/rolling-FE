cfda33e560f1db9efeac17317bf39196cf5821be
ss <tt@email.com>
2025-02-05 17:58:25 +0900

    chore: 시작 환경 세팅 중

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,18 +1,22 @@
-import Header from "../components/Header/Header";
+import HeaderService from "../components/HeaderService/HeaderService";
+import HeaderLogoOnly from "../components/Header/HeaderLogoOnly";
 
 function Post() {
   return (
-    <div style={{ paddingTop: "100px" }}>
-      <Header />
-      <h1>Post 테스트</h1>
-      <p>Post</p>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
+    <div>
+      <HeaderLogoOnly />
+      <HeaderService />
+      <div style={{ paddingTop: "133px" }}>
+        <h1>Post 테스트</h1>
+        <p>Post</p>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+        <div style={{ height: "200px" }}>안녕</div>
+      </div>
     </div>
   );
 }
