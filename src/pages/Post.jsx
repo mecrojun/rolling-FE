7d31556a9c614d2a690589e9b6ff564ee3225ccb
heresue <sue.synpark@gmail.com>
2025-02-06 07:23:32 +0900

    chore: Post 페이지 파일 생성

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,18 +1,27 @@
-import Header from "../components/Header/Header";
+import HeaderLogoOnly from "../components/Header/HeaderLogoOnly";
+import Colorchip from "../components/Colorchip";
+import PrimaryButton from "../components/Buttons/PrimaryButton";
+import ToggleButton from "../components/Buttons/ToggleButton";
 
 function Post() {
   return (
     <div style={{ paddingTop: "100px" }}>
-      <Header />
-      <h1>Post 테스트</h1>
-      <p>Post</p>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
-      <div style={{ height: "200px" }}>안녕</div>
+      <HeaderLogoOnly />
+      <div>
+        <div>
+          {/* 섹션 타이틀 태그 정하기 (/list 민지님) */}
+          <h2>To.</h2>
+          {/* <InputField /> */}
+        </div>
+        <div>
+          <h2>배경화면 선택</h2>
+          <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+          <ToggleButton width="122px" height="40px" />
+          {/* Colorchip 컴포넌트 수정 - 아이콘, 이미지 */}
+          <Colorchip />
+        </div>
+        <PrimaryButton>생성하기</PrimaryButton>
+      </div>
     </div>
   );
 }
