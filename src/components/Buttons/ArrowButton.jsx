7c0422e5abfef41239f2cb723e4015b4b04c02e8
mecrojun <71626627+mecrojun@users.noreply.github.com>
2025-02-07 15:47:48 +0900

    Refactor/Button & Feat/Main page (#29)
    * feat: button 컴포넌트 기능 구현
    
    * Revert "feat: button 컴포넌트 기능 구현"
    
    This reverts commit 7724adb628d2f636456e20938f2ff0e447814ce8.
    
    * refactor: svgr을 이용한 버튼 컴포넌트 코드 재작성
    
    * refactor: 버튼 컴포넌트, 함수명 변경 및 불필요 코드 정리
    
    * 버튼 컴포넌트명 수정
    
    * fix: props 오류 해결
    
    * chore: 컴포넌트.js -> 컴포넌트.jsx 로 파일명 변경
    
    * refactor: outlinedButton의 함수 내부 내용 가독성 개선
    
    * refactor: OutlinedButton 함수명 변경
    
    * refactor: ToggleButton props 정리
    
    * refactor: 버튼 컴포넌트 내용 기능과 스타일로 분리
    
    * chore: 메인페이지 생성 및 라우터 설정
    
    * refactor: 버튼 컴포넌트 props: onClick 추가 및 메인페이지 프레임 제작
    
    * primaryButton padding 삭제
    
    * main.jsx -> Main.jsx 파일명 수정
    
    * chore: Main.jsx 로 파일명 변경
    
    ---------
    
    Co-authored-by: Seok-jun.Kang <52899088+KSJ27@users.noreply.github.com>

--- src/components/Buttons/ArrowButton.jsx
+++ src/components/Buttons/ArrowButton.jsx
@@ -1,9 +1,9 @@
 import * as ArrowButtonStyle from "./ArrowButton.style";
 
-function ArrowButton({ direction }) {
+function ArrowButton({ direction, onClick }) {
 
     return (
-      <ArrowButtonStyle.ArrowButtonWrapper>
+      <ArrowButtonStyle.ArrowButtonWrapper onClick={onClick}>
         {ArrowButtonStyle.returnArrowImg(direction)}
       </ArrowButtonStyle.ArrowButtonWrapper>
     );
