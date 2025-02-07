57845d12bdbbab2475366b71aa3257e417baebe4
ss <tt@email.com>
2025-02-07 18:20:59 +0900

    feat: recipients api 적용 및 더미 데이터 삭제

--- src/components/HeaderService/HeaderService.jsx
+++ src/components/HeaderService/HeaderService.jsx
@@ -1,20 +1,38 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import To from "./To";
 import Reaction from "./Reaction";
 import ShareButton from "./ShareButton";
 import ProfileImages from "./ProfileImages";
 import AddEmoji from "./AddEmoji";
 import * as H from "./HeaderService.style";
-import { useState } from "react";
-import { mockRecipient } from "./MockData";
 
 function HeaderService() {
-  const [recipient, setRecipient] = useState(mockRecipient);
+  const { recipientId } = useParams();
+  const [recipient, setRecipient] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipientData = async () => {
+      try {
+        const response = await axios.get(
+          `https://rolling-api.vercel.app/1-7/recipients/9817/`
+        );
+        setRecipient(response.data);
+      } catch (error) {
+        console.error(" 대상 정보 조회 오류:", error);
+      }
+    };
+
+    fetchRecipientData();
+  }, [recipientId]);
+
+  if (!recipient) return null;
 
   return (
     <H.HeaderBox>
       <H.HeaderContainer>
         <To name={recipient.name} />
-
         <H.RightSection>
           <ProfileImages
             recentMessages={recipient.recentMessages}
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,10 @@
-import Header from "../components/Header/Header";
-
+import HeaderLogoOnly from "../components/Header/HeaderLogoOnly";
+import HeaderService from "../components/HeaderService/HeaderService";
 function Post() {
   return (
-    <div style={{ paddingTop: "100px" }}>
-      <Header />
+    <div>
+      <HeaderLogoOnly />
+      <HeaderService />
       <h1>Post 테스트</h1>
       <p>Post</p>
       <div style={{ height: "200px" }}>안녕</div>
