78d8cf1d13076299387b811f9c2e39f2783dfee4
Seok-jun.Kang <52899088+KSJ27@users.noreply.github.com>
2025-02-08 17:48:38 +0900

    Feat/edit page (#35) - 중간 병합

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Post from "../pages/Post";
 import List from "../pages/list";
 import Main from "../pages/main";
+import Edit from "../pages/Edit/Edit";
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/post" element={<Post />} />
+        <Route path="/post/:id/edit" element={<Edit />} />
         <Route path="/list" element={<List />} />
       </Routes>
     </BrowserRouter>
