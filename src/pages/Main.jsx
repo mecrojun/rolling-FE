d1a3995559b6f521faa2cf653c0a05e029fd38f7
mecrojun <wnsgh6588@naver.com>
2025-02-09 20:43:33 +0900

    feat: 메인페이지 기능

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header/Header";
-import PrimaryButton from "../components/Buttons/PrimaryButton";
-import { Content, H2, Label, Line, P, Wrapper } from "./Main.style";
+import { Content, H2, Label, Line, MoveListButton, P, Wrapper } from "./Main.style";
 import { useNavigate } from "react-router-dom";
 
 function Main() {
@@ -19,7 +18,7 @@ function Main() {
             <Wrapper className="txt-box-01">
               <Label>Point. 01</Label>
               <Wrapper className="txt-area">
-                <H2>누구나 손쉽게, 온라인<br />
+                <H2>누구나 손쉽게, 온라인 <br />
                   롤링 페이퍼를 만들 수 있어요</H2>
                 <P>로그인 없이 자유롭게 만들어요.</P>
               </Wrapper>
@@ -32,7 +31,7 @@ function Main() {
             <Wrapper className="txt-box-02">
               <Label>Point. 02</Label>
               <Wrapper className="txt-area">
-                <H2>서로에게 이모지로 감정을<br />
+                <H2>서로에게 이모지로 감정을 <br />
                 표현해보세요</H2>
                 <P>롤링 페이퍼에 이모지를 추가할 수 있어요.</P>
               </Wrapper>
@@ -42,7 +41,7 @@ function Main() {
             </Wrapper>
           </Content>
         </Wrapper>
-        <Wrapper className="buttonWrapper"><PrimaryButton width="280px" onClick={handlePrimaryButton}>구경해보기</PrimaryButton></Wrapper>
+        <Wrapper className="buttonWrapper"><MoveListButton onClick={handlePrimaryButton}>구경해보기</MoveListButton></Wrapper>
       </Wrapper>
     );
   }
