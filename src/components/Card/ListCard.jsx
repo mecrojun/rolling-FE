eaa1c01d5b5a1211a980658ed6ddb2eb6d404045
MinJi Cho <jam96013@gmail.com>
2025-02-10 18:25:25 +0900

    ListCard components 글자색이 배경 밝기에 따라 변경되도록 수정

--- src/components/Card/ListCard.jsx
+++ src/components/Card/ListCard.jsx
@@ -1,6 +1,32 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import * as LC from "./ListCardStyle.js";
 import { EmojiBadge } from "../Badge/Badge";
+
+const getLuminance = (r, g, b) => 0.299 * r + 0.587 * g + 0.114 * b;
+
+const getImageLuminance = (url, callback) => {
+  const img = new Image();
+  img.crossOrigin = "Anonymous";
+  img.src = url;
+
+  img.onload = () => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    let total = 0;
+
+    for (let i = 0; i < data.length; i += 4) {
+      total += getLuminance(data[i], data[i + 1], data[i + 2]);
+    }
+
+    callback(total / (data.length / 4));
+  };
+};
 
 function ProfileList({ recentMessages = [], messageCount = 0 }) {
   const maxVisibleProfiles = 3;
@@ -37,9 +63,16 @@ function Card({
   topReactions,
 }) {
   const Navigate = useNavigate();
+  const [isDark, setIsDark] = useState(false);
   const handleCardClick = () => {
     Navigate(`/post/${id}`);
   };
+
+  useEffect(() => {
+    if (backgroundImageURL) {
+      getImageLuminance(backgroundImageURL, (lum) => setIsDark(lum < 140));
+    }
+  }, [backgroundImageURL]);
 
   return (
     <LC.CardContainer
@@ -49,12 +82,12 @@ function Card({
       style={{ cursor: "pointer" }}
     >
       <LC.CardContent>
-        <LC.CardTitle>To. {name}</LC.CardTitle>
+        <LC.CardTitle $isDark={isDark}>To. {name}</LC.CardTitle>
         <ProfileList
           recentMessages={recentMessages}
           messageCount={messageCount}
         />
-        <LC.CardCountText>
+        <LC.CardCountText $isDark={isDark}>
           <LC.CountBoldText>{messageCount}</LC.CountBoldText>명이 작성했어요!
         </LC.CardCountText>
 
