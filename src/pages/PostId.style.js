8dbeddc224687503fb327d4404b05668d73f5b68
ss <tt@email.com>
2025-02-11 12:41:19 +0900

    design: PostId 반응형 적용

--- src/pages/PostId.style.js
+++ src/pages/PostId.style.js
@@ -30,9 +30,20 @@ export const MessageCardBox = styled.div`
   margin: 0 auto;
   padding-bottom: 50px;
 
-  @media (max-width: 1248px) {
-    width: calc(100% - 48px);
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  @media (max-width: 1250px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 24px;
+    max-width: 800px;
+  }
+
+  @media (max-width: 900px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 28px;
+    width: 100%;
   }
 `;
 
